12a0352efcd1a2104ebec89678b6e65b0fd41f3e
Danilo <77410497+DaniloRds@users.noreply.github.com>
2023-03-19 15:54:34 -0300

    Update start.bat

--- UnityCleanV2/Base/start.bat
+++ UnityCleanV2/Base/start.bat
@@ -1,3 +1,12 @@
 @echo off
-artifacts\FXServer.exe +exec config/config.cfg +set onesync_enableInfinity 0 +set onesync_population false +set onesync on +set sv_enforceGameBuild 2612
+
+echo ===-------------------------------===
+echo     Base Unity Free V2 (2.1.2)
+echo     Developed by: Unity
+echo     Discord: discord.gg/kYFy8JwVfd
+echo ===-------------------------------===
+
+pause
+start ..\artifacts\FXServer.exe +exec config/config.cfg +set onesync_enableInfinity 0 +set onesync_population false +set onesync on +set sv_enforceGameBuild 2612
+exit
 
