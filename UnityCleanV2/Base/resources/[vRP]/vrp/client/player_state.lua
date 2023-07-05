a0ebe7564a54450afbbc81bb614ad8b22986dcdd
Danilo <77410497+DaniloRds@users.noreply.github.com>
2023-07-05 11:47:56 -0300

    Update player_state.lua

--- UnityCleanV2/Base/resources/[vRP]/vrp/client/player_state.lua
+++ UnityCleanV2/Base/resources/[vRP]/vrp/client/player_state.lua
@@ -1,8 +1,18 @@
 local state_ready = false
+local acao = false
 
 function tvRP.playerStateReady(state)
 	state_ready = state
 end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		if IsPlayerPlaying(PlayerId()) and state_ready then
+			vRPserver._updateWeapons(tvRP.getWeapons())
+		end
+	end
+end)
 
 Citizen.CreateThread(function()
 	while true do
@@ -11,7 +21,6 @@ Citizen.CreateThread(function()
 			local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(),true))
 			vRPserver._updatePos(x,y,z)
 			vRPserver._updateHealth(tvRP.getHealth())
-			vRPserver._updateWeapons(tvRP.getWeapons())
 			vRPserver._updateCustomization(tvRP.getCustomization())
 			vRPserver._updateArmor(tvRP.getArmour())
 		end
@@ -20,112 +29,119 @@ end)
 
 RegisterNetEvent('save:database')
 AddEventHandler('save:database',function()
-    if not inRound6 and IsPlayerPlaying(PlayerId()) and state_ready and not in_arena then
-        local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
-        vRPserver._updatePos(x,y,z)
-        vRPserver._updateHealth(tvRP.getHealth())
-        vRPserver._updateWeapons(tvRP.getWeapons())
-        vRPserver._updateCustomization(tvRP.getCustomization())
-        vRPserver._updateArmor(tvRP.getArmour())
-    end
+	if IsPlayerPlaying(PlayerId()) and state_ready then
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+		vRPserver._updatePos(x,y,z)
+		vRPserver._updateHealth(tvRP.getHealth())
+		vRPserver._updateWeapons(tvRP.getWeapons())
+		vRPserver._updateCustomization(tvRP.getCustomization())
+		vRPserver._updateArmor(tvRP.getArmour())
+	end
 end)
 
-
 local weapon_types = {
-"WEAPON_KNIFE",
-"WEAPON_KNUCKLE",
-"WEAPON_HAMMER",
-"WEAPON_BAT",
-"WEAPON_GOLFCLUB",
-"WEAPON_CROWBAR",
-"WEAPON_BOTTLE",
-"WEAPON_DAGGER",
-"WEAPON_HATCHET",
-"WEAPON_MACHETE",
-"WEAPON_SWITCHBLADE",
-"WEAPON_PROXMINE",
-"WEAPON_BZGAS",
-"WEAPON_SMOKEGRENADE",
-"WEAPON_MOLOTOV",
-"WEAPON_FIREEXTINGUISHER",
-"WEAPON_PETROLCAN",
-"WEAPON_SNOWBALL",
-"WEAPON_FLARE",
-"WEAPON_BATTLEAXE",
-"WEAPON_STONE_HATCHET",
-"GADGET_PARACHUTE",
-"WEAPON_BALL",
-"WEAPON_REVOLVER",
-"WEAPON_POOLCUE",
-"WEAPON_PIPEWRENCH",
-"WEAPON_PISTOL",
-"WEAPON_PISTOL_MK2",
-"WEAPON_COMBATPISTOL",
-"WEAPON_APPISTOL",
-"WEAPON_SNSPISTOL",
-"WEAPON_HEAVYPISTOL",
-"WEAPON_VINTAGEPISTOL",
-"WEAPON_FLAREGUN",
-"WEAPON_MARKSMANPISTOL",
-"WEAPON_MICROSMG",
-"WEAPON_MINISMG",
-"WEAPON_SMG",
-"WEAPON_SMG_MK2",
-"WEAPON_ASSAULTSMG",
-"WEAPON_MG",
-"WEAPON_COMBATMG",
-"WEAPON_COMBATMG_MK2",
-"WEAPON_COMBATPDW",
-"WEAPON_GUSENBERG",
-"WEAPON_MACHINEPISTOL",
-"WEAPON_ASSAULTRIFLE",
-"WEAPON_ASSAULTRIFLE_MK2",
-"WEAPON_CARBINERIFLE",
-"WEAPON_CARBINERIFLE_MK2",
-"WEAPON_ADVANCEDRIFLE",
-"WEAPON_SPECIALCARBINE",
-"WEAPON_BULLPUPRIFLE",
-"WEAPON_COMPACTRIFLE",
-"WEAPON_PUMPSHOTGUN",
-"WEAPON_SWEEPERSHOTGUN",
-"WEAPON_SAWNOFFSHOTGUN",
-"WEAPON_BULLPUPSHOTGUN",
-"WEAPON_ASSAULTSHOTGUN",
-"WEAPON_MUSKET",
-"WEAPON_HEAVYSHOTGUN",
-"WEAPON_DBSHOTGUN",
-"WEAPON_FIREWORK",
-"WEAPON_STICKYBOMB",
-"WEAPON_SNSPISTOL_MK2",
-"WEAPON_REVOLVER_MK2",
-"WEAPON_DOUBLEACTION",
-"WEAPON_SPECIALCARBINE_MK2",
-"WEAPON_BULLPUPRIFLE_MK2",
-"WEAPON_RAYPISTOL",
-"WEAPON_ASSAULTRIFLE_MK2",
-"WEAPON_HEAVYSNIPER_MK2",
-"WEAPON_MARKSMANRIFLE_MK2",
-"WEAPON_PUMPSHOTGUN_MK2",
-"WEAPON_CARBINERIFLE",
-"WEAPON_STUNGUN",
-"WEAPON_PISTOL50",
-"WEAPON_NIGHTSTICK",
-"WEAPON_FLASHLIGHT",
-"WEAPON_RAILGUN",
-"WEAPON_RAYCARBINE",
-"WEAPON_GRENADE",
-"WEAPON_PIPEBOMB",
-"WEAPON_RPG",
-"WEAPON_GRENADELAUNCHER",
-"WEAPON_MINIGUN",
-"WEAPON_HOMINGLAUNCHER",
-"WEAPON_COMPACTLAUNCHER",
-"WEAPON_RAYMINIGUN",
-"WEAPON_SNIPERRIFLE",
-"WEAPON_HEAVYSNIPER",
-"WEAPON_HEAVYSNIPER_MK2",
-"WEAPON_AUTOSHOTGUN",
-"WEAPON_GRENADELAUNCHER_SMOKE",
+	"WEAPON_DAGGER",
+	"WEAPON_BAT",
+	"WEAPON_BOTTLE",
+	"WEAPON_CROWBAR",
+	"WEAPON_FLASHLIGHT",
+	"WEAPON_GOLFCLUB",
+	"WEAPON_HAMMER",
+	"WEAPON_WEAPON_HATCHET",
+	"WEAPON_WEAPON_KNUCKLES",
+	"WEAPON_KNIFE",
+	"WEAPON_MACHETE",
+	"WEAPON_SWITCHBLADE",
+	"WEAPON_NIGHTSTICK",
+	"WEAPON_WHENCH",
+	"WEAPON_BATTLEAXE",
+	"WEAPON_POOLCUE",
+	"WEAPON_STONE_HATCHET",
+
+	"WEAPON_PISTOL",
+	"WEAPON_PISTOL_MK2",
+	"WEAPON_COMBATPISTOL",
+	"WEAPON_APPISTOL",
+	"WEAPON_STUNGUN",
+	"WEAPON_PISTOL50",
+	"WEAPON_SNSPISTOL",
+	"WEAPON_SNSPISTOL_MK2",
+	"WEAPON_HEAVYPISTOL",
+	"WEAPON_VINTAGEPISTOL",
+	"WEAPON_FLAREGUN",
+	"WEAPON_MARKSMANPISTOL",
+	"WEAPON_REVOLVER",
+	"WEAPON_REVOLVER_MK2",
+	"WEAPON_DOUBLEACTION",
+	"WEAPON_RAYPISTOL",
+	"WEAPON_CERAMICPISTOL",
+	"WEAPON_NAVYREVOLVER",
+
+	"WEAPON_MICROSMG",
+	"WEAPON_SMG",
+	"WEAPON_SMG_MK2",
+	"WEAPON_ASSAULTSMG",
+	"WEAPON_COMBATPDW",
+	"WEAPON_MACHINEPISTOL",
+	"WEAPON_MINISMG",
+	"WEAPON_RAYCARBINE",
+
+	"WEAPON_PUMPSHOTGUN",
+	"WEAPON_PUMPSHOTGUN_MK2",
+	"WEAPON_SAWNOFFSHOTGUN",
+	"WEAPON_ASSAULTSHOTGUN",
+	"WEAPON_BULLPUPSHOTGUN",
+	"WEAPON_MUSKET",
+	"WEAPON_HEAVYSHOTGUN",
+	"WEAPON_DBSHOTGUN",
+	"WEAPON_AUTOSHOTGUN",
+
+	"WEAPON_ASSAULTRIFLE",
+	"WEAPON_ASSAULTRIFLE_MK2",
+	"WEAPON_CARBINERIFLE",
+	"WEAPON_CARBINERIFLE_MK2",
+	"WEAPON_ADVANCEDRIFLE",
+	"WEAPON_SPECIALCARBINE",
+	"WEAPON_SPECIALCARBINE_MK2",
+	"WEAPON_BULLPUPRIFLE",
+	"WEAPON_BULLPUPRIFLE_MK2",
+	"WEAPON_COMPACTRIFLE",
+
+	"WEAPON_MG",
+	"WEAPON_COMBATMG",
+	"WEAPON_COMBATMG_MK2",
+	"WEAPON_GUSENBERG",
+
+	"WEAPON_SNIPERRIFLE",
+	"WEAPON_HEAVYSNIPER",
+	"WEAPON_HEAVYSNIPER_MK2",
+	"WEAPON_MASKMANRIFLE",
+	"WEAPON_MASKMANRIFLE_MK2",
+
+	"WEAPON_RPG",
+	"WEAPON_GRENADELAUNCHER",
+	"WEAPON_GRENADELAUNCHER_SMOKE",
+	"WEAPON_MINIGUN",
+	"WEAPON_FIREWORK",
+	"WEAPON_RAILGUN",
+	"WEAPON_HOMINGLAUNCHER",
+	"WEAPON_COMPACTLAUNCHER",
+	"WEAPON_RAYMINIGUN",
+
+	"WEAPON_GRANADE",
+	"WEAPON_BZGAS",
+	"WEAPON_MOLOTOV",
+	"WEAPON_STICKYBOMB",
+	"WEAPON_PROXMINE",
+	"WEAPON_SNOWBALL",
+	"WEAPON_PIPEBOMB",
+	"WEAPON_BALL",
+	"WEAPON_SMOKEGRENADE",
+	"WEAPON_FLARE",
+
+	"WEAPON_PETROLCAN",
+	"GADGET_PARACHUTE",
+	"WEAPON_FIREEXTINGUISHER"
 }
 
 function tvRP.getWeapons()
@@ -146,8 +162,6 @@ function tvRP.getWeapons()
 			end
 		end
 	end
-	tvRP.legalWeaponsChecker(weapons)
-
 	return weapons
 end
 
@@ -161,41 +175,18 @@ function tvRP.giveWeapons(weapons,clear_before)
 	local player = PlayerPedId()
 	if clear_before then
 		RemoveAllPedWeapons(player,true)
-		weapon_list = {}
 	end
 
 	for k,weapon in pairs(weapons) do
 		local hash = GetHashKey(k)
 		local ammo = weapon.ammo or 0
 		GiveWeaponToPed(player,hash,ammo,false)
-		weapon_list[k] = weapon
 	end
-end
-
-function tvRP.getWeaponsLegal()
-	return weapon_list
-end
-
-function tvRP.legalWeaponsChecker(weapon)
-	local source = source
-	local weapon = weapon
-	local weapons_legal = tvRP.getWeaponsLegal()
-	local ilegal = false
-	for v, b in pairs(weapon) do
-	  if not weapon_list[v] then
-		ilegal = true 
-	  end
-	end
-	if ilegal then
-	  tvRP.giveWeapons(weapons_legal, true)
-		weapon = {}
-		TriggerServerEvent("AC:ac_Armas:ACC", source)
-	end
-	return weapon
 end
 
 function tvRP.setArmour(amount)
-	SetPedArmour(PlayerPedId(),amount)
+    SetPedArmour(PlayerPedId(),amount)
+    TriggerEvent("10100052854mgre",amount)
 end
 
 function tvRP.getArmour()
@@ -247,7 +238,7 @@ function tvRP.setCustomization(custom)
 	local r = async()
 	Citizen.CreateThread(function()
 		if custom then
-			local ped = PlayerPedId()
+			local ped = GetPlayerPed(-1)
 			local mhash = nil
 
 			if custom.modelhash then
@@ -257,25 +248,27 @@ function tvRP.setCustomization(custom)
 			end
 
 			if mhash then
-				local i = 0
-				while not HasModelLoaded(mhash) and i < 10000 do
-					RequestModel(mhash)
-					Citizen.Wait(10)
-				end
+                local i = 0
+                while not HasModelLoaded(mhash) and i < 10000 do
+                    i = i + 1
+                    RequestModel(mhash)
+                    Citizen.Wait(10)
+                end
 
-				if HasModelLoaded(mhash) then
-					local weapons = tvRP.getWeapons()
-					local armour = GetPedArmour(ped)
-					local health = tvRP.getHealth()
-					SetPlayerModel(PlayerId(),mhash)
-					tvRP.setHealth(health)
-					tvRP.giveWeapons(weapons,true)
-					tvRP.setArmour(armour)
-					SetModelAsNoLongerNeeded(mhash)
-				end
-			end
+                if HasModelLoaded(mhash) then
+                    local weapons = tvRP.getWeapons()
+                    local armour = GetPedArmour(ped)
+                    local health = tvRP.getHealth()
 
-			ped = PlayerPedId()
+                    SetPlayerModel(PlayerId(),mhash)
+                    tvRP.setHealth(health)
+                    tvRP.giveWeapons(weapons,true)
+                    tvRP.setArmour(armour)
+                    SetModelAsNoLongerNeeded(mhash)
+                end
+            end
+
+			ped = GetPlayerPed(-1)
 			SetPedMaxHealth(ped,400)
 
 			for k,v in pairs(custom) do
@@ -297,4 +290,20 @@ function tvRP.setCustomization(custom)
 		r()
 	end)
 	return r:wait()
+end
+
+function tvRP.blockAcao()
+	acao = true
+end
+
+function tvRP.desblockAcao()
+	acao = false
+end
+
+function tvRP.checkAcao()
+	if acao then
+		return true
+	else
+		return false
+	end
 end
