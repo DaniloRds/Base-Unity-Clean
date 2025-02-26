3d6b5ddcc1ab12af969dcf2a3ffc8c4c96b722ef
Danilo <77410497+DaniloRds@users.noreply.github.com>
2025-02-26 20:22:07 -0300

    3.0.2

--- UnityVrpex/Base/resources/[Core]/[Player]/vrp_notifyitens/ui/app.js
+++ UnityVrpex/Base/resources/[Core]/[Player]/vrp_notifyitens/ui/app.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 		var html = "";
 
 		if (event.data.mode == 'sucesso') {
-			html = '<div class="fundo" style="border: 1px solid rgba(4, 231, 64, 0.767);" ><div class="fundofalso"><img src="http://localhost:80/cidade/inventory/'+event.data.item+'.png" alt=""></div><div class="fundoimg"></div><br><br><br><br>'
+			html = '<div class="fundo" style="border: 1px solid rgba(4, 231, 64, 0.767);" ><div class="fundofalso"><img src="https://r2.fivemanage.com/3f14FTnNqMTYPNd23NBRQ/images/'+event.data.item+'.png" alt=""></div><div class="fundoimg"></div><br><br><br><br>'
 		}
 
 		if (event.data.mode == 'negado') {			
-			html = '<div class="fundo" style="border: 1px solid rgba(231, 15, 4, 0.77);"><div class="fundofalso"><img src="http://localhost:80/cidade/inventory/'+event.data.item+'.png" alt=""></div><div class="fundoimg"></div><br><br><br><br>'
+			html = '<div class="fundo" style="border: 1px solid rgba(231, 15, 4, 0.77);"><div class="fundofalso"><img src="https://r2.fivemanage.com/3f14FTnNqMTYPNd23NBRQ/images/'+event.data.item+'.png" alt=""></div><div class="fundoimg"></div><br><br><br><br>'
 		}
 
 		$(html).fadeIn(500).appendTo("#notifyitens").delay(5000).fadeOut(500);
